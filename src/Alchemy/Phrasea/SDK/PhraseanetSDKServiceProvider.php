cbf2bc50ae4f7bac36b77f68ce92b76477d2550c
Romain Neutron <imprec@gmail.com>
2013-06-04 13:19:45 +0200

    Pimp cache

--- src/Alchemy/Phrasea/SDK/PhraseanetSDKServiceProvider.php
+++ src/Alchemy/Phrasea/SDK/PhraseanetSDKServiceProvider.php
@@ -11,115 +11,94 @@
 
 namespace Alchemy\Phrasea\SDK;
 
-use Alchemy\Phrasea\SDK\Exception\RuntimeException;
-use Doctrine\Common\Cache\ArrayCache;
-use Doctrine\Common\Cache\MemcacheCache;
-use Doctrine\Common\Cache\MemcachedCache;
+use Alchemy\Phrasea\SDK\CacheFactory;
+//use Alchemy\Phrasea\SDK\Cache\CachePlugin;
+use PhraseanetSDK\Exception\RuntimeException;
 use Guzzle\Cache\DoctrineCacheAdapter;
-use Guzzle\Plugin\Cache\CachePlugin as GuzzleCachePlugin;
+use Guzzle\Http\Client as Guzzle;
+use Guzzle\Plugin\Cache\CachePlugin;
 use PhraseanetSDK\Client;
+use PhraseanetSDK\HttpAdapter\Guzzle as GuzzleAdapter;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use PhraseanetSDK\HttpAdapter\Guzzle as Adapter;
+use Guzzle\Plugin\Cache\SkipRevalidation;
+use Guzzle\Plugin\Log\LogPlugin;
+use Guzzle\Log\PsrLogAdapter;
+use Monolog\Handler\NullHandler;
 
 /**
  * Phraseanet SDK Silex provider
  */
 class PhraseanetSDKServiceProvider implements ServiceProviderInterface
 {
-
     public function register(Application $app)
     {
+        $app['phraseanet-sdk.cache-factory'] = $app->share(function (Application $app) {
+            return new CacheFactory();
+        });
+
+        if (!isset($app['monolog.handler'])) {
+            $app['monolog.handler'] = function () use ($app) {
+                return new NullHandler();
+            };
+        }
+
+        $app['phraseanet-sdk.cache-adapter'] = $app->share(function (Application $app) {
+            $host = isset($app['phraseanet-sdk.cache_host']) ? $app['phraseanet-sdk.cache_host'] : null;
+            $port = isset($app['phraseanet-sdk.cache_port']) ? $app['phraseanet-sdk.cache_port'] : null;
+            $type = isset($app['phraseanet-sdk.cache']) ? $app['phraseanet-sdk.cache'] : 'array';
+
+            try {
+                $cache = $app['phraseanet-sdk.cache-factory']->create($type, $host, $port);
+                $app['monolog']->debug(sprintf('Phraseanet SDK using cache %s %s', $type, (($host||$port) ? ' with parameters '.$host.':'.$port : '')));
+            } catch (RuntimeException $e) {
+                $app['monolog']->error(sprintf('Error while instancing cache : %s', $e->getMessage()));
+
+                $cache = $app['phraseanet-sdk.cache-factory']->create('array', null, null);
+            }
+
+            return new DoctrineCacheAdapter($cache);
+        });
+
+        $app['phraseanet-sdk.guzzle-client'] = $app->share(function (Application $app) {
+            $guzzle = new Guzzle($app['phraseanet-sdk.apiUrl']);
+
+            $lifetime = isset($app['phraseanet-sdk.cache_ttl']) ? $app['phraseanet-sdk.cache_ttl'] : 360;
+
+            $guzzle->addSubscriber(new CachePlugin(array(
+                'adapter'      => $app['phraseanet-sdk.cache-adapter'],
+                'default_ttl'  => $lifetime,
+                'revalidation' => new SkipRevalidation(),
+            )));
+            $guzzle->addSubscriber(new LogPlugin(new PsrLogAdapter($app['monolog'])));
+
+            return $guzzle;
+        });
+
         $app['phraseanet-sdk'] = $app->share(function() use ($app) {
+            $client = new Client($app['phraseanet-sdk.apiKey'], $app['phraseanet-sdk.apiSecret'], new GuzzleAdapter($app['phraseanet-sdk.guzzle-client']), $app['monolog']);
 
-                $guzzle = $app['guzzle.client'];
-                /* @var $guzzle \Guzzle\Http\Client */
+            if (isset($app['phraseanet-sdk.apiDevToken'])) {
+                $client->setAccessToken($app['phraseanet-sdk.apiDevToken']);
+            }
 
-                $getCache = function () use ($app) {
-
-                    if(!isset($app['phraseanet-sdk.cache'])) {
-                        return new ArrayCache();
-                    }
-
-                    switch(strtolower($app['phraseanet-sdk.cache'])) {
-                        case 'array':
-                            return new ArrayCache();
-                            break;
-                        case 'memcache':
-                            $memcache = new \Memcache();
-
-                            $host = $app['phraseanet-sdk.cache_host'];
-                            $port = $app['phraseanet-sdk.cache_port'];
-
-                            $memcache->addServer($host,$port);
-
-                            $cache = new MemcacheCache();
-                            $cache->setMemcache($memcache);
-
-                            return $cache;
-                            break;
-                        case 'memcached':
-                            $memcached = new \Memcached();
-
-                            $host = $app['phraseanet-sdk.cache_host'];
-                            $port = $app['phraseanet-sdk.cache_port'];
-
-                            $memcached->addServer($host,$port);
-
-                            $cache = new MemcachedCache();
-                            $cache->setMemcached($memcached);
-
-                            return $cache;
-                            break;
-                        default:
-                            throw new RuntimeException(sprintf('Cache `%s` is not supported', $app['phraseanet-sdk.cache']));
-                            break;
-                    }
-                };
-
-                $adapter = new DoctrineCacheAdapter($getCache());
-
-                $lifetime = isset($app['phraseanet-sdk.cache_ttl']) ? $app['phraseanet-sdk.cache_ttl'] : 60;
-                $revalidate = isset($app['phraseanet-sdk.cache_revalidate']) ? $app['phraseanet-sdk.cache_revalidate'] : null;
-
-                $guzzle->addSubscriber(new GuzzleCachePlugin($adapter, $lifetime));
-
-                $guzzle->addSubscriber(new CachePlugin($lifetime, $revalidate));
-
-                $guzzle->setBaseUrl($app['phraseanet-sdk.apiUrl']);
-
-                $logger = isset($app['monolog']) && $app['debug'] ? $app['monolog'] : null;
-
-                $client = new Client($app['phraseanet-sdk.apiKey'], $app['phraseanet-sdk.apiSecret'], new Adapter($guzzle), $logger);
-
-                if (isset($app['phraseanet-sdk.apiDevToken'])) {
-                    $client->setAccessToken($app['phraseanet-sdk.apiDevToken']);
-                }
-
-                return $client;
-            });
+            return $client;
+        });
     }
 
     public function boot(Application $app)
     {
-        if ( ! isset($app['phraseanet-sdk.apiSecret'])) {
+        if (!isset($app['phraseanet-sdk.apiSecret'])) {
             throw new RuntimeException('You must provide an api secret');
         }
-
-        if ( ! isset($app['phraseanet-sdk.apiKey'])) {
+        if (!isset($app['phraseanet-sdk.apiKey'])) {
             throw new RuntimeException('You must provide an api key');
         }
-
-        if ( ! isset($app['phraseanet-sdk.apiUrl'])) {
+        if (!isset($app['phraseanet-sdk.apiUrl'])) {
             throw new RuntimeException('You must provide an api url');
         }
-
-        if ( ! isset($app['monolog'])) {
+        if (!isset($app['monolog'])) {
             throw new RuntimeException('Phraseanet SDK Provider requires monolog service');
-        }
-
-        if ( ! isset($app['guzzle'])) {
-            throw new RuntimeException('Phraseanet SDK Provider requires guzzle service');
         }
     }
 }
--- tests/Alchemy/Tests/Phrasea/SDK/SilexProviderTest.php
+++ tests/Alchemy/Tests/Phrasea/SDK/SilexProviderTest.php
@@ -3,18 +3,64 @@
 namespace Alchemy\Tests\Phrasea\SDK;
 
 use Alchemy\Phrasea\SDK\PhraseanetSDKServiceProvider;
-use Monolog\Handler\StreamHandler;
+use Monolog\Handler\NullHandler;
 use Silex\Application;
 use Silex\Provider\MonologServiceProvider;
 
 class SilexProviderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testInitialize()
+    /**
+     * @dataProvider provideServices
+     */
+    public function testServices($name, $instanceOf)
     {
         $app = $this->getConfiguredApplication();
         $app->register(new PhraseanetSDKServiceProvider());
 
-    	$this->assertInstanceOf('PhraseanetSDK\Client', $app['phraseanet-sdk']);
+    	$this->assertInstanceOf($instanceOf, $app[$name]);
+    }
+
+    public function provideServices()
+    {
+        return array(
+            array('phraseanet-sdk', 'PhraseanetSDK\Client'),
+            array('phraseanet-sdk.cache-factory', 'Alchemy\Phrasea\SDK\CacheFactory'),
+            array('phraseanet-sdk.cache-adapter', 'Guzzle\Cache\DoctrineCacheAdapter'),
+            array('phraseanet-sdk.guzzle-client', 'Guzzle\Http\ClientInterface'),
+        );
+    }
+
+    /**
+     * @dataProvider provideCacheConfigurations
+     */
+    public function testACacheProviderIsAlwaysReturned($type, $instanceOf, $errors, $infos)
+    {
+        $app = $this->getConfiguredApplication();
+        $app->register(new PhraseanetSDKServiceProvider());
+
+        $app['monolog'] = $this->getMockBuilder('Monolog\Logger')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $app['monolog']->expects($this->exactly($errors))
+            ->method('error');
+        $app['monolog']->expects($this->exactly($infos))
+            ->method('debug');
+
+        $app['phraseanet-sdk.cache_host'] = null;
+        $app['phraseanet-sdk.cache_port'] = null;
+        $app['phraseanet-sdk.cache'] = $type;
+
+        $this->assertInstanceOf($instanceOf, $app['phraseanet-sdk.cache-adapter']->getCacheObject());
+    }
+
+    public function provideCacheConfigurations()
+    {
+        return array(
+            array('array', 'Doctrine\Common\Cache\ArrayCache', 0, 1),
+            array('memcache', 'Doctrine\Common\Cache\MemcacheCache', 0, 1),
+            array('memcached', 'Doctrine\Common\Cache\MemcachedCache', 0, 1),
+            array('unknown', 'Doctrine\Common\Cache\ArrayCache', 1, 0),
+        );
     }
 
     private function getConfiguredApplication()
@@ -26,7 +72,7 @@ class SilexProviderTest extends \PHPUnit_Framework_TestCase
         $app['phraseanet-sdk.apiUrl'] = 'https://bidule.net';
 
         $app->register(new MonologServiceProvider());
-        $app['monolog.handler'] = new StreamHandler('php://stdout');
+        $app['monolog.handler'] = new NullHandler();
 
         return $app;
     }
